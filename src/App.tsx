071a9ba61ecbbd2116fcf7bd73f8b22dd2e5d9de
aga87 <aga.labonarska@outlook.com>
2022-01-31 15:15:31 +0100

    Add normalize, reset and global styles with Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Normalize } from 'styled-normalize';
+import { createGlobalStyle } from 'styled-components';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html, body {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const App = () => (
+  <div>
+    <Normalize />
+    <GlobalStyle />
+  </div>
+);
 
 export default App;
